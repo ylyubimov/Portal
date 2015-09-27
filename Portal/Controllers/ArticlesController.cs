d4a24f454815270374edac7d0aece12d41e3f9a0
Anna.Savinova <anna.savinova@phystech.edu>
2015-09-27 14:11:01 +0300

    [Articles] Added articles controller and view

---
+++ Portal/Controllers/ArticlesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portal.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        public class Article
+        {
+            public int id = 0;
+            public String author = "author";
+            public String text = "text";
+
+            public Article(int _id)
+            {
+                id = _id;
+            }
+        }
+        // GET: Articles
+        public ActionResult Index(int? id)
+        {
+            if (id != null)
+            {
+                return View(new Article((int)id));
+            }
+            else
+            {
+                return View(new Article(-1));
+            }
+        }
+    }
+}
