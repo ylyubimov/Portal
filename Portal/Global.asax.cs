992fd5f541d49a4f9878e28f6f212b756fb80353
AnnaSavinova <anna.savinova@phystech.edu>
2015-10-05 23:41:32 +0300

    Models in Articles and Blogs

--- Portal/Global.asax.cs
+++ Portal/Global.asax.cs
@@ -14,7 +14,7 @@ namespace Portal
         protected void Application_Start()
         {
             //// Инициализация для создания базы
-            Database.SetInitializer(new DropCreateDatabaseAlways<ApplicationDbContext>());
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<ApplicationDbContext>());
             using (var db = new ApplicationDbContext())
             {
                 if (!db.Database.Exists())
