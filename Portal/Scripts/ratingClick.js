d4441cbf922c07c6854dc3a951fbab75646b5dcf
AnnaSavinova <anna.savinova@phystech.edu>
2015-11-04 22:15:10 +0300

    Теперь у нас можно лайкать!

---
+++ Portal/Scripts/ratingClick.js
@@ -0,0 +1,29 @@
+﻿function OnLikeClick(url, id) {
+    $.ajax({
+        url: url,
+        method: 'POST',
+        dataType: 'json',
+        success: function (data) {
+            var likesCount = document.getElementById("LikesCount" + id);
+            likesCount.innerText = data.likesCount;
+        },
+        error: function () {
+            alert("Не удалось поставить лайк, возможно, вам недостаточно нравится.");
+        }
+    });
+}
+
+function OnDislikeClick(url, id) {
+    $.ajax({
+        url: url,
+        method: 'POST',
+        dataType: 'json',
+        success: function (data) {
+            var dislikesCount = document.getElementById("DislikesCount" + id);
+            dislikesCount.innerText = data.dislikesCount;
+        },
+        error: function () {
+            alert("Не удалось поставить дислайк, возможно, вы недостаточно недовольны.");
+        }
+    });
+}
