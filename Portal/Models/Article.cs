4ee7d0ed99816a23ffa0779888407eda36715112
Nikolai <nvvaulin@gmail.com>
2015-11-24 22:40:43 +0300

    delete program view and controller

--- Portal/Models/Article.cs
+++ Portal/Models/Article.cs
@@ -9,12 +9,13 @@ namespace Portal.Models
     {
         [Key]
         public int ID { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Название обязателено")]
         public string Name { get; set; }
         public DateTime? Date_of_Creation { get; set; }
         public int? Likes_Count { get; set; } 
         public int? Dislikes_Count { get; set; }
         [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "Контент обязателен")]
         public string Text { get; set; }
         public virtual ICollection<Blog> Blogs { get; set; }
         [Required]
