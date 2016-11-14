17bd49f0663736df49c4197037df45ab787c4ba5
paulin-mipt <paulin.mipt@gmail.com>
2016-11-15 02:58:54 +0300

    send mail to an approved teacher user

--- Portal/Controllers/AdminController.cs
+++ Portal/Controllers/AdminController.cs
@@ -7,6 +7,9 @@ using System.Web.Mvc;
 using Portal.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Net.Mail;
+using System.Collections.Specialized;
+using System.Configuration;
 
 namespace Portal.Controllers
 {
@@ -85,6 +88,53 @@ namespace Portal.Controllers
             return RedirectToAction( "AdminTable" );
         }
 
+        protected void SendEmailToVerificatedUser(Person user)
+        {
+            try
+            {
+                NameValueCollection mailingSection = (NameValueCollection)ConfigurationManager.GetSection("adminMailingSettings");
+
+                string senderEmail = mailingSection["FromEmailAddress"].ToString(); // address that makes all the mailing
+                string senderPasswd = mailingSection["FromEmailPassword"].ToString();
+                string senderDisplayName = mailingSection["FromEmailDisplayName"].ToString();
+
+                MailMessage mail = new MailMessage();
+                mail.To.Add(user.Email);
+                mail.From = new MailAddress(senderEmail, senderDisplayName, System.Text.Encoding.UTF8);
+                mail.Subject = "[ABBYY Portal] You were approved as a teacher";
+                mail.SubjectEncoding = System.Text.Encoding.UTF8;
+
+                string bodyTemplate = "Hello, {0}!\r\n\r\n" +
+                "You were just approved as a teacher and now you are able to start working on courses.\r\n\r\n" +
+                "Yours, ABBYY Portal Team.";
+                string fullName = String.Format("{0} {1} {2}", user.First_Name, user.Middle_Name, user.Second_Name);
+
+                mail.Body = String.Format(bodyTemplate, fullName);
+                mail.BodyEncoding = System.Text.Encoding.UTF8;
+                mail.IsBodyHtml = false;
+                mail.Priority = MailPriority.High;
+                SmtpClient client = new SmtpClient();
+                client.Credentials = new System.Net.NetworkCredential(senderEmail, senderPasswd);
+                client.Port = 587;
+                client.Host = "smtp.gmail.com";
+                client.EnableSsl = true;
+
+                client.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                Exception ex2 = ex;
+                List<string> errorMessages = new List<string>();
+                while (ex2 != null)
+                {
+                    errorMessages.Add(ex2.ToString());
+                    ex2 = ex2.InnerException;
+                }
+                IdentityResult result = new IdentityResult(errorMessages);
+                // should log it somewhere
+            }
+        }
+
         [Authorize( Roles = "admin" )]
         [HttpPost]
         public ActionResult ChangeType( string id )
@@ -92,12 +142,18 @@ namespace Portal.Controllers
             Person person = db.Users.Where( p => id == p.Id ).FirstOrDefault();
 
             if( person.Person_Type == "Teacher" ) {
-                person.Is_Waiting_Approval = false;
                 person.Person_Type = "Admin";
             } else if( person.Person_Type == "Admin" ) {
                 person.Person_Type = "Student";
             } else {
-                person.Is_Waiting_Approval = false;
+                if (person.Is_Waiting_Approval)
+                {
+                    person.Is_Waiting_Approval = false;
+                    person.Person_Type = "Teacher";
+                    db.SaveChanges();
+                    SendEmailToVerificatedUser(person);
+                    return RedirectToAction("AdminTable");
+                }
                 person.Person_Type = "Teacher";
             }
             db.SaveChanges();
